5ded6d106ab362ae49672175c5276a0eaaeb8360
Elexis Blash <elexisblash@gmail.com>
2022-07-11 20:32:25 -0400

    Fixing heroku

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 
 const PORT = process.env.PORT || 3001;
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budgetTracker";
 
 const app = express();
 
@@ -24,7 +24,7 @@ mongoose.connect(MONGODB_URI, {
 // routes
 app.use(require("./routes/api.js"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budgetTracker", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
